5b6e074577fbf5decf8cdc075532f55ac17121e0
Jogno Junior <jvj.eng18@uea.edu.br>
2024-06-10 21:25:41 -0400

    ADD Redis on NestJS

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,11 +7,17 @@ import { dbConfigOptions } from './config/database.config';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './models/auth/auth.module';
 import { UserModule } from './models/users/users.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [TypeOrmModule.forRootAsync(dbConfigOptions),
     ConfigModule.forRoot({
       envFilePath: '.security.env',
+    }),
+    CacheModule.register({
+      store: 'redisStore',
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
     }),
     MovieModule,
     AuthModule,
--- api/src/models/movies/movie.service.ts
+++ api/src/models/movies/movie.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, Type } from "@nestjs/common";
+import { Inject, Injectable, Type } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { FindManyOptions, Repository, FindOneOptions } from "typeorm";
 import { Movie } from "./movie.entity";
 import { MovieCreateDTO } from "./movie-create.dto";
 import { instanceToPlain, plainToInstance } from "class-transformer";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
 
 @Injectable()
 
@@ -11,6 +13,7 @@ export class MovieService {
     constructor(
         @InjectRepository(Movie)
         private movieRepository: Repository<Movie>,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache,
     ){}
 
     dtoToEntity(entity: Type<any>, data: any): any {
@@ -19,16 +22,32 @@ export class MovieService {
 
 
     async getAll(): Promise <Movie[]> {
+        const cacheKey = 'movies';
+        const cachedItens = await this.cacheManager.get<Movie[]>(cacheKey);
+        if (cachedItens) {
+            return cachedItens;
+        }
+
         const options: FindManyOptions<Movie> = {
         order: {
             title: 'ASC',
             }
         };
-        return this.movieRepository.find(options);
+        const movies = this.movieRepository.find(options);
+        await this.cacheManager.set(cacheKey, movies, 300);
+        return movies;
+
+        
 
     }
 
     async getOne(id: number): Promise<Movie> {
+        const cacheKey = `movie-${id}`;
+        const cachedItem = await this.cacheManager.get<Movie>(cacheKey);
+        if (cachedItem) {
+            return cachedItem;
+        }
+
         const options: FindOneOptions<Movie> = {
             where: { id },
         };
@@ -37,24 +56,37 @@ export class MovieService {
         if (!movie) {
             throw new Error('Movie not found');
         }
+
+        await this.cacheManager.set(cacheKey, movie, 300);
+
         return movie;
     }
 
-    async update(id: number, movie: Movie): Promise<Movie> {
+    async update(id: number, movieData: Movie): Promise<Movie> {
         if (id == 0) throw new Error('Invalid ID');
-
-        await this.getOne(id);
+    
+        const movie = await this.getOne(id);
+        const updatedMovie = { ...movie, ...movieData};
+    
         await this.movieRepository.update(id, movie);
-        return this.getOne(id);
+        await this.cacheManager.del(`movie-${id}`);
+        await this.cacheManager.del('movies');
+    
+        return updatedMovie as Movie;
     }
 
     async create(movie: MovieCreateDTO): Promise<Movie> {
         const movieEntity: Movie = this.dtoToEntity(Movie, movie) as Movie;
-        return this.movieRepository.save(movieEntity);
+        
+        const newM =  this.movieRepository.save(movieEntity);
+        await this.cacheManager.del('movies');
+        return newM;
     }
 
     async delete(id: number): Promise<void> {
         await this.getOne(id);
         await this.movieRepository.delete(id);
+        await this.cacheManager.del(`movie-${id}`);
+        await this.cacheManager.del('movies');
     }
 }
